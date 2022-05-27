2a2b5564d9238e89cf85cc94027146f37d1168b2
sheikh ariful <sheikharifulislam770@gmail.com>
2022-05-28 00:56:36 +0600

    Complete slider functionality

--- src/views/components/home/slider/HeaderSlider.jsx
+++ src/views/components/home/slider/HeaderSlider.jsx
@@ -1,18 +1,61 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 import { NavLink } from "react-router-dom";
 
 const HeaderSlider = () => {
     const [sliderData, setSliderData] = useState([]);
+    const [count, setCount] = useState(0);
+    const timeOutRef = useRef(null);
+
+    const resetTimeOut = () => {
+        if (timeOutRef.current) {
+            clearTimeout(timeOutRef.current);
+        }
+    };
+
+    const incrementCount = () => {
+        if (count < sliderData.length - 1 || 0) {
+            setCount((previousState) => previousState + 1);
+        } else {
+            setCount(0);
+        }
+    };
+
+    const decrementCount = () => {
+        if (count > 0) {
+            setCount((previousState) => previousState - 1);
+        } else {
+            setCount(sliderData.length - 1 || 0);
+        }
+    };
+
     useEffect(() => {
         axios.get("/sliderData.json").then((data) => {
             setSliderData(data.data);
-            console.log(data.data);
         });
     }, []);
+
+    useEffect(() => {
+        resetTimeOut();
+        timeOutRef.current = setTimeout(() => {
+            if (count < sliderData.length - 1 || 0) {
+                setCount((previousState) => previousState + 1);
+            } else {
+                setCount(0);
+            }
+        }, 3000);
+        return () => {
+            resetTimeOut();
+        };
+    }, [count, sliderData]);
+
     return (
         <div className="w-full  overflow-hidden">
-            <div className="w-[500%] h-full flex ">
+            <div
+                className="w-[500%] h-full flex relative top-0 duration-300"
+                style={{ right: count * 100 + "%" }}
+            >
                 {sliderData.map((data) => (
                     <NavLink
                         to="/"
@@ -26,6 +69,20 @@ const HeaderSlider = () => {
                         />
                     </NavLink>
                 ))}
+            </div>
+            <div>
+                <button
+                    className="slider-arrow left-5"
+                    onClick={decrementCount}
+                >
+                    <AiOutlineLeft size={20} />
+                </button>
+                <button
+                    className="slider-arrow right-5"
+                    onClick={incrementCount}
+                >
+                    <AiOutlineRight size={20} />
+                </button>
             </div>
         </div>
     );

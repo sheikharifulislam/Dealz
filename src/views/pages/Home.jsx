19d3ff757465cec4fe26f63d2e45013da17b857f
sheikh ariful <sheikharifulislam770@gmail.com>
2022-05-30 23:43:43 +0600

    add swiper

--- src/views/pages/Home.jsx
+++ src/views/pages/Home.jsx
@@ -1,7 +1,8 @@
-import React from "react";
-import Categories from "../components/home/categories/Categories";
-import Products from "../components/home/products/Products";
-import HeaderSlider from "../components/home/slider/HeaderSlider";
+import React from 'react'
+import ReviewTestimonial from '../components/custom/review testimonial/ReviewTestimonial'
+import Categories from '../components/home/categories/Categories'
+import Products from '../components/home/products/Products'
+import HeaderSlider from '../components/home/slider/HeaderSlider'
 
 const Home = () => {
     return (
@@ -9,8 +10,9 @@ const Home = () => {
             <HeaderSlider />
             <Categories />
             <Products />
+            <ReviewTestimonial />
         </>
-    );
-};
+    )
+}
 
-export default Home;
+export default Home

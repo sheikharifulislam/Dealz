e41f62b4f94b62661c547b6985a49b383488a9b0
sheikh ariful <sheikharifulislam770@gmail.com>
2022-06-07 01:56:22 +0600

    complete product details image section and product main info section

--- src/views/components/product details/ProductDetails.jsx
+++ src/views/components/product details/ProductDetails.jsx
@@ -1,24 +1,24 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import {
     AiOutlineHome,
     AiOutlineInfoCircle,
     AiOutlineMinus,
     AiOutlinePlus,
-} from 'react-icons/ai';
-import { BsCashCoin } from 'react-icons/bs';
-import { IoLocationOutline } from 'react-icons/io5';
-import { MdOutlineSecurity } from 'react-icons/md';
-import { TbTruckReturn } from 'react-icons/tb';
-import StarRating from '../../../lib/star rating/StarRating';
-import offerRange from '../../../utils/offerRange';
+} from "react-icons/ai";
+import { BsCashCoin } from "react-icons/bs";
+import { IoLocationOutline } from "react-icons/io5";
+import { MdOutlineSecurity } from "react-icons/md";
+import { TbTruckReturn } from "react-icons/tb";
+import StarRating from "../../../lib/star rating/StarRating";
+import offerRange from "../../../utils/offerRange";
 
 const ProductDetails = () => {
     const [product, setProduct] = useState({});
 
     useEffect(() => {
         axios
-            .get('/productDetails.json')
+            .get("/productDetails.json")
             .then((response) => {
                 setProduct(response.data);
             })
@@ -31,7 +31,7 @@ const ProductDetails = () => {
         <section className="cs_container rounded border bg-white p-8 shadow-md">
             <div>
                 <div className="flex justify-between">
-                    <div className="w-[30%]">
+                    <div className="w-[27%]">
                         <div className="h-[330px] border-b border-gray-300 pb-2">
                             {Object.keys(product).length >= 1 && (
                                 <img
@@ -52,7 +52,7 @@ const ProductDetails = () => {
                             ))}
                         </div>
                     </div>
-                    <div className="w-[40%]">
+                    <div className="w-[37%]">
                         <div className="border-b-2 border-gray-100 pb-2">
                             <div>
                                 <h2>{product.productName}</h2>
@@ -91,65 +91,81 @@ const ProductDetails = () => {
                             <div className="item-center flex">
                                 {product.colors?.map((color, index) => (
                                     <div
-                                        className="h-8 w-8 rounded-[50%]"
+                                        className="mr-2 mb-1 h-8 w-8 cursor-pointer rounded-full"
                                         style={{ backgroundColor: color }}
                                         key={index}
                                     ></div>
                                 ))}
                             </div>
                         </div>
-                        <div>
+                        <div className="mt-1">
                             <h4>Size</h4>
                             <div>
                                 {product.size?.map((size, index) => (
                                     <div
                                         key={index}
-                                        className="mr-2 inline-block border px-4 py-1"
+                                        className="mr-2 mb-1 inline-block cursor-pointer rounded border px-4 py-1"
                                     >
                                         {size.toUpperCase()}
                                     </div>
                                 ))}
                             </div>
                         </div>
-                        <div className="">
-                            <h4>Quantity </h4>
+                        <div className="item-center mt-3 flex">
+                            <h4 className="mr-4">Quantity </h4>
                             <div className="flex items-center">
-                                <AiOutlinePlus />
+                                <button className="rounded bg-gray-200 p-2">
+                                    <AiOutlinePlus />
+                                </button>
                                 <input
                                     type="number"
-                                    className="mx-2 w-12 border-hidden  bg-gray-100 p-0 text-center"
+                                    className="mx-2 w-12 border-hidden p-0 text-center"
                                     name="quantity"
                                     defaultValue="1"
                                 />
-                                <AiOutlineMinus />
+                                <button className="rounded bg-gray-200 p-2">
+                                    <AiOutlineMinus />
+                                </button>
                             </div>
                         </div>
-                        <div>
-                            <button>Add To Cart</button>
-                            <button>Buy Now</button>
+                        <div className="item-center mt-5 flex justify-between  ">
+                            <button className="w-[49%] rounded bg-[#2abbe8] py-3 text-white">
+                                Buy Now
+                            </button>
+                            <button className="w-[49%] rounded bg-[#f57224] py-3 text-white">
+                                Add To Cart
+                            </button>
                         </div>
                     </div>
-                    <div className="w-[25%]">
-                        <div>
-                            <div>
+                    <div className="w-[30%] rounded bg-gray-50 py-3 px-2">
+                        <div className="border-b-2 border-gray-100 pb-2">
+                            <div className="mb-2 flex items-center justify-between">
                                 <h4>Delivery</h4>
                                 <AiOutlineInfoCircle />
                             </div>
-
-                            <div>
-                                <IoLocationOutline />
-                                <span>
-                                    Dhaka, Dhaka North, Banani Road No. 12 - 19
-                                </span>
-                                <button>Change</button>
-                            </div>
-                            <div>
-                                <AiOutlineHome />
-                                <div>
-                                    <span>Home Delivery</span>
-                                    <small>3-5 days</small>
+                            <div className="mb-2 flex items-start border-b pb-2">
+                                <IoLocationOutline size={22} />
+                                <div className="flex w-[90%] items-center">
+                                    <span className="text-sm">
+                                        Dhaka, Dhaka North, Banani Road No. 12 -
+                                        19
+                                    </span>
+                                    <button className="text-xs uppercase text-[#1a9cb7]">
+                                        Change
+                                    </button>
                                 </div>
-                                <span>$50</span>
+                            </div>
+                            <div className="item-center flex">
+                                <AiOutlineHome size={22} />
+                                <div className="ml-1 flex-1">
+                                    <span className="block text-sm">
+                                        Home Delivery
+                                    </span>
+                                    <small className="block text-xs">
+                                        3-5 days
+                                    </small>
+                                </div>
+                                <span className="">$50</span>
                             </div>
                             <div>
                                 <BsCashCoin />
